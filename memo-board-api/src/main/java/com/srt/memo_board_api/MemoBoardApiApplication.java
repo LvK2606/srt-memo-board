1eb0c5e1565df3f5797a0320210ebdd61cfd546f
LvK2606 <lekhoa20042606@gmail.com>
2026-07-06 10:30:50 +0700

    chore: initialize memo-board-api with Spring Boot

---
+++ memo-board-api/src/main/java/com/srt/memo_board_api/MemoBoardApiApplication.java
@@ -0,0 +1,13 @@
+package com.srt.memo_board_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MemoBoardApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MemoBoardApiApplication.class, args);
+	}
+
+}
